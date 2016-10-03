02179b8559c8af91e90ddea38e4e4ab10da01162
andmikus <atsarginsi@hotmail.com>
2016-10-04 01:54:58 +0300

    First commit

--- Documents/www/Paysera-test/spec/CommissionSpec.php
+++ Documents/www/Paysera-test/spec/CommissionSpec.php
@@ -8,14 +8,20 @@ use Prophecy\Argument;
 
 class CommissionSpec extends ObjectBehavior
 {
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Commission::class);
     }
 
-    function it_should_return_min_commission()
+    function it_should_limit_min_commission()
     {
-        $this->limitAmount('cash_in', 'natural', 10)->shouldReturn(10);
+        $this->limitMinAmount('cash_out', 'juridical', 0.3, 'EUR')->shouldBeLike(0.5);
+    }
+
+    function it_should_limit_max_commission()
+    {
+        $this->limitMaxAmount('cash_in', 'natural', 10, 'EUR')->shouldBeLike( 5);
     }
 
 }
